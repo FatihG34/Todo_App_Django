6ea45f3ac9425fac56108b78dc6fee8ee246ec2b
FatihG34 <fthgnydn34@gmail.com>
2022-08-29 20:36:11 +0300

    models and user info modified

--- todo/models.py
+++ todo/models.py
@@ -15,7 +15,7 @@ class Todo(models.Model):
     ]
     title=models.CharField(max_length=70)
     description = models.TextField()
-    status = models.CharField(, max_length=2, choices=status_choices)
+    status = models.CharField( max_length=2, choices=status_choices)
     priority = models.CharField(max_length=2, choices=priority_choices)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
