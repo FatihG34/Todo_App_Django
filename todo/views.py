84274d91dce29a09073ec6e8cff6fb352364f90d
FatihG34 <fthgnydn34@gmail.com>
2022-08-29 20:49:40 +0300

    templates created

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from todo.models import Todo
+
 def home(request):
-    return HttpResponse('Welcome')
+    todos = Todo.objects.all()
+    context = {
+        'todos':todos
+    }
+    return render(request,'todo/home.html', context)
